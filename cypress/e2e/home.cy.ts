50ab3f33f8199341fdc33ff9d219e6161747c561
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-05-23 13:19:07 +0200

    Added context of tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,15 +3,27 @@ context("Home page", () => {
         beforeEach(() => {
             cy.visit("http://localhost:8888")
         })
-        it("should be h1 in document", () => {
-          cy.get("[data-reactid='.0.0.0']").contains("todos")
+
+        context("Todo list don't exist", () => {
+            it("should not display the list", () => {
+            cy.get("todo-list li").should("not.exist")
+            cy.get('[data-reactid=".0.2"]').should("not.exist")
+            })
         })
 
-        it("should add new todo list", () => {
-          cy.get('input[placeholder="What needs to be done?"]').type("Buy bread{enter}" ,)
-          cy.get('li').eq(0).contains("Buy bread")
-          cy.get('input[placeholder="What needs to be done?"]').type("Finish the book I'm currently reading{enter}")
-          cy.get('li').eq(1).contains("Finish the book I'm currently reading")
+        context("Todo list exist", () => {
+            it("should be h1 in document", () => {
+            cy.get("[data-reactid='.0.0.0']").contains("todos")
+            })
+            it("should add new todo list", () => {
+            cy.get('input[placeholder="What needs to be done?"]').type("Buy bread{enter}" ,)
+            cy.get('li').eq(0).contains("Buy bread")
+            cy.get(".todo-list li").should("have.length", 1)
+            cy.get('input[placeholder="What needs to be done?"]').type("Finish the book I'm currently reading{enter}")
+            cy.get('li').eq(1).contains("Finish the book I'm currently reading")
+            cy.get(".todo-list li").should("have.length", 2)
+            cy.get('[data-reactid=".0.2"]').should("exist")
+            })
         })
     })
 })
